4fbb5d08db690dee6611631af596a2482512b2d1
rohola <hilbert.cantor@gmail.com>
2022-03-08 18:04:22 -0700

    adding abbreviation and acronym pronunciation. breaks work

--- README.md
+++ README.md
@@ -49,5 +49,108 @@ Note that when you use ```model_tag``` the value for ```model_file``` should be 
 But ```model_file``` can be used with ```vocoder_tag``` or you can just leave vocoder specification empty.
 If you use ```none``` the algorithm will be the ```griffin-lim``` vocoder.
 
+
+### Abbreviations and acronyms
+The code supports adding abbreviations and acronyms to the default cmu dictionary in runtime. 
+- For abbreviations like IDC which should be read as "I don't care" you should first use the following code to get the phonemes.
+```
+>> from g2p_en import G2p
+>> g2p = G2p()
+>> g2p("I don't care")
+['AY1', ' ', 'D', 'OW1', 'N', 'T', ' ', 'K', 'EH1', 'R']
+```
+and add it to abbreviations.json file. 
+- For acronyms like NFL just use the tag as follows:
+```
+The <say-as interpret-as='acronym'> AMD </say-as> stands for Advanced Micro Devices.
+```
+
+- For Roman numerals and other characters you can follow the rules of phonemes. For example "IV" can be extracted 
+by feeding "four" to the g2p and then adding it to the abbreviations.json file.
+- For other words with no corresponding phonemes, first lookup IPA pronunciation in wikipedia then you can use the following code to get the phonemes.
+```
+>> from arpabetandipaconvertor.phoneticarphabet2arpabet import PhoneticAlphabet2ARPAbetConvertor
+>> ipa_convertor = PhoneticAlphabet2ARPAbetConvertor()
+>> ipa_convertor.convert('hæmedɑ:n')
+HH AE0 M EY0 D AA0 N
+```
+This doesn't work perfectly in all cases. So for correction see the table below.
+
+- Vowels
+
+1-letter	|2-letter		|  IPA	| Example	                    |   ARPAbet
+----------|------------|------|-----------------------------------------------------|-----------
+a	   		|    AA		|   ɑ 	|  balm 	      |   B AA1 M
+@	   		|    AE	  	|   æ 	|  bat   	              |   B AE1 T
+A	   		|    AH	  	|   ʌ 	|  butt  	                |   B AH1 T
+c    		|    AO	  	|   ɔ	|  bought	           |   B AO1 T
+W	   		|    AW	  	|   aʊ	|   bout             |   B AW1 T
+x	   		|    AX	  	|   ə	|   about          |   AH0 B AW1 T
+N/A	   		|    AXR	|   ɚ 	|   letter            |   L EH1 T ER0
+Y	   		|    AY	 	|   aɪ	|   bite              |   B AY1 T
+E	   		|    EH	 	|   ɛ	|   bet              |   B EH1 T
+R    		|    ER	 	|   ɝ	|   bird           |   B ER1 D
+e	    	|    EY		|   eɪ	|   bait              |   B EH1 T
+I	   		|    IH	  	|   ɪ	|   bit           |   B IH1 T
+X	   		|    IX	  	|   ɨ	|   roses           |   R OW1 Z
+i	    	|    IY	 	|   i	|   beat             |   B IY1 T
+o	    	|    OW	  	|   oʊ	|   boat         |   B OW1 T
+O	    	|    OY	 	|   ɔɪ	|   boy                 |   B OY1
+U       	|	  UH    	|   ʊ	|   book              |   B UH1 K
+u	    	|    UW		|   u	|   boot            |   B UW1 T
+N/A	    	|    UX  	|   ʉ	|   dude          |  D UW1 D
+
+
+- Consonants
+
+1-letter	|  2-letter    	| IPA	|  Example                   |  ARPABET
+----------|----------------|-----|--------------------------------------------------|---------
+b	  		|   B	       	|  b  	|  buy            |  B AY1
+C	    	|   CH	       	|  tʃ 	|  China    |  CH AY1 N AH0
+d	    	|   D	       	|  d	|  die           |  D AY1
+D	    	|   DH	       	|  ð	|  thy              |  DH AY1
+F	    	|   DX	       	|  ɾ	|  butter       |  B AH1 T ER0
+L	    	|   EL	       	|  l̩	|  bottle         |  B AA1 T AH0 L
+M	    	|   EM	       	|  m̩	|  rhythm       |  R IH1 DH AH0 M
+N	    	|   EN	       	|  n̩	|  button         |  B AH1 T AH0 N
+f	    	|   F	       	|  f	|  fight       |  F AY1 T
+g	    	|   G	       	|  ɡ	|  guy        |  G AY1
+h	    	|   HH or H 	|  h  	|	 high            |  HH AY1
+J	    	|   JH	       	|  dʒ 	|  jive           |  JH AY1 V
+k	    	|   K	       	|  k	|  kite             |  K AY1 T
+l	    	|   L	       	|  l	|  lie             |  L AY1
+m	    	|   M	       	|  m	|  my              |  M AY1
+n       	|	N	       	|  n	|  nigh           |  N AY1
+G	    	|   NX or NG|  ŋ  	|	 sing          |  S IH1 NG
+N/A     	|	NX   		|  ɾ̃	|  winner            |  W IH1 N ER0
+p	    	|   P	       	|  p	|  pie            |  P AY1
+Q	    	|   Q	       	|  ʔ	|  uh-oh            |
+r	    	|   R	       	|  ɹ	|  rye       |  R AY1
+s	    	|   S          	|  s	|  sigh         |  S AY1
+S	    	|   SH	       	|  ʃ  	|  shy           |  SH AY1
+t	    	|   T	       	|  t	|  tie            |  T AY1
+T	    	|   TH	       	|  θ	|  thigh           |  TH AY1
+v       	|	V	       	|  v  	|  vie         |  V AY1
+w	    	|   W	       	|  w	|  wise         |  W AY1 Z
+H	    	|   WH	       	|  ʍ	|  why      |  HH W AY1, W AY1
+y	    	|   Y	       	|  j	|  yachting       |  Y AA1 T IH0 NG
+z	    	|   Z	       	|  z	|  zoo              |  Z UW1
+Z	    	|   ZH	       	|  ʒ	|  pleasure     |  P L EH1 ZH ER0
+
+### Breaks (Pauses)
+The code supports adding pauses (breaks) to the speech. It can be specified in seconds or milliseconds.
+
+```
+"This is the first sentence. <break time='2000ms'/> This is the second sentence. <break time='1s'/> This is the third sentence."
+```
+
+(no breaks allowed in the beginning or end of the sentence)
+
 ### TODO:
-We will add ryan models as downloadable tags soon.
+- We will add ryan models as downloadable tags soon.
+
+
+- Detect pronounceable sequences of characters [this](https://stackoverflow.com/questions/40209592/arranging-letters-in-the-most-pronounceable-way) 
+and generating unprounacable gibberish for dataset [here]()
+
+- Add '.' at the end of acronyms for a better pronunciation
--- voice_synthesizer.py
+++ voice_synthesizer.py
@@ -6,6 +6,37 @@ import os
 import torch
 from espnet2.bin.tts_inference import Text2Speech
 from scipy.io.wavfile import write
+import ast
+from bs4 import BeautifulSoup
+
+
+character_to_phonemes = {"A": ['EY2'],
+                         "B": ['B', 'IY2'],
+                         "C": ['S', 'IY1'],
+                         "D": ['D', 'IY1'],
+                         "E": ['IY1'],
+                         "F": ['EH1', 'F'],
+                         "G": ['JH', 'IY1'],
+                         "H": ['EY1', 'CH'],
+                         "I": ['AY1'],
+                         "J": ['JH', 'EY1'],
+                         "K": ['K', 'EY1'],
+                         "L": ['EH1', 'L'],
+                         "M": ['EH1', 'M'],
+                         "N": ['EH1', 'N'],
+                         "O": ['OW1'],
+                         "P": ['P', 'IY1'],
+                         "Q": ['K', 'Y', 'UW1'],
+                         "R": ['AA1', 'R'],
+                         "S": ['EH1', 'S'],
+                         "T": ['T', 'IY1'],
+                         "U": ['Y', 'UW1'],
+                         "V": ['V', 'IY1'],
+                         "W": ['D', 'AH1', 'B', 'AH0', 'Y', 'UW0'],
+                         "X": ['EH1', 'K', 'S'],
+                         "Y": ['W', 'AY1'],
+                         "Z": ['Z', 'IY1'],
+                         }
 
 class TTSConfig:
 
@@ -14,7 +45,8 @@ class TTSConfig:
                  model_tag=None,
                  vocoder_file=None,
                  vocoder_tag=None,
-                 voice_dir="",
+                 voice_dir=None,
+                 abbreviations_file=None,
                  fs="",
                  port="",
                  device="cuda"
@@ -24,6 +56,7 @@ class TTSConfig:
         self.vocoder_file = vocoder_file
         self.vocoder_tag = vocoder_tag
         self.voice_dir = voice_dir
+        self.abbreviations_file = abbreviations_file
         self.fs = fs
         self.port = port
         self.device = device
@@ -88,6 +121,47 @@ class VoiceSynthesizer:
             )
         else:
             raise Exception("The model_file or model_tag is not provided")
+
+        self.add_abreviations_to_phonemes()
+
+        # custom_dict = {
+        #     "co": [['K', 'AA2', 'L', 'ER0', 'AA1', 'D', 'OW0']],
+        #     "fugafuga": [['AAAAAAAAAAAAAAAAAA']]
+        # }
+
+        # # initialize g2p instance
+        # self.text2speech.preprocess_fn.tokenizer.g2p("initialize")
+        # # update dict
+        # for word in custom_dict:
+        #     if word in self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu:
+        #         self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu[word].insert(0, custom_dict[word])
+        #     else:
+        #         self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu[word] = custom_dict[word]
+        # self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu.update(custom_dict)
+
+    def add_abreviations_to_phonemes(self):
+        abbreviations_dict = json.loads(open(self.config.abbreviations_file).read())
+        try:
+            abbreviations_dict = {k.lower(): [ast.literal_eval(v)] for k, v in abbreviations_dict.items()}
+        except Exception as e:
+            print(e)
+            raise Exception("The abbreviations file is not in the correct format")
+        # initialize g2p instance
+        self.text2speech.preprocess_fn.tokenizer.g2p("initialize")
+        # update dict
+        for word in abbreviations_dict:
+            if word in self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu:
+                self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu[word].insert(0, abbreviations_dict[word][0])
+            else:
+                self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu[word] = abbreviations_dict[word]
+
+    def add_acronyms_to_phonemes_dict(self, acronym, phonemes):
+        # initialize g2p instance
+        self.text2speech.preprocess_fn.tokenizer.g2p("initialize")
+        if acronym in self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu:
+            self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu[acronym].insert(0, phonemes)
+        else:
+            self.text2speech.preprocess_fn.tokenizer.g2p.g2p.cmu[acronym] = [phonemes]
 
     def extract_phonemes(self, text, cmu_syle=True):
         phonemes = self.text2speech.preprocess_fn.tokenizer.text2tokens(text)
@@ -162,9 +236,140 @@ class VoiceSynthesizer:
         print(f"wav type: {type(wav)}")
         return {"phonemes": " ".join(lines), "wav": wav.view(-1).cpu().numpy()}
 
+    def parse_input_text(self, input_text):
+        # there should be a better way to do this
+        soup = BeautifulSoup(input_text, "html.parser")
+        parsed_text = soup.prettify().split('\n')
+        parsed_text = list(filter(lambda item: not item.startswith('</') and item != '', parsed_text))
+        return parsed_text
+
+    def get_text(self, input_text):
+        soup = BeautifulSoup(input_text, "html.parser")
+        return soup.get_text()
+
+    def _convert_time_to_miliseconds(self, break_time):
+        if break_time.endswith('ms'):
+            break_time = float(break_time[:-2])
+        elif break_time.endswith('s'):
+            break_time = int(break_time[:-1]) * 1000
+        else:
+            raise ValueError("Unsupported break time format")
+
+        return break_time
+
+    def _flatten(self, t):
+        return [item for sublist in t for item in sublist]
+
+    def tts2(self, input_text):
+        #parsed_input_text = self.parse_input_text(input_text)
+
+        # extract acronyms and add it to the cmu dictionary
+        soup = BeautifulSoup(input_text, "html.parser")
+        for s in soup.select('say-as'):
+            if s.attrs['interpret-as'] == 'acronym':
+                characters = list(filter(lambda item: item.strip(), s.text))
+                acronym = ''.join(characters).lower()
+                phonemes = self._flatten([character_to_phonemes[character.upper()] for character in characters])
+                self.add_acronyms_to_phonemes_dict(acronym, phonemes)
+                s.replace_with(s.text)
+
+        # concatenate all consecutive strings
+        soup = BeautifulSoup(str(soup), "html.parser")
+        outputs = []
+        all_y = []
+        all_durations = []
+        for s in soup.children:
+            if s.name == 'break':
+                break_time = self._convert_time_to_miliseconds(s['time'])
+                outputs.append({"break": break_time})
+            else:
+                text = s.get_text()
+                text = re.sub(' +', ' ', text) # remove multiple spaces
+                with torch.no_grad():
+                    output = self.text2speech(text)
+                    y = output['wav'].view(-1).cpu().tolist()
+                all_y.append(y)
+                durations = output['duration'].cpu().tolist()[1:]
+                all_durations.append(durations)
+                outputs.append({"speech": output})
+
+
+
+        # for i in range(len(parsed_input_text)):
+        #     if parsed_input_text[i].startswith('<say-as interpret-as="acronym">'):
+        #         characters = list(filter(lambda item: item.strip(), parsed_input_text[i+1]))
+        #         acronym = ''.join(characters)
+        #         phonemes = self._flatten([character_to_phonemes[character.upper()] for character in characters])
+        #         self.add_acronyms_to_phonemes_dict(acronym, phonemes)
+
+        # synthesis
+        # with torch.no_grad():
+        #     outputs = []
+        #     all_y = []
+        #     all_durations = []
+        #     for i in range(len(parsed_input_text)):
+        #         if parsed_input_text[i].startswith('<break'):
+        #             break_time = BeautifulSoup(parsed_input_text[i], "html.parser").find("break").attrs['time']
+        #             break_time = self._convert_time_to_miliseconds(break_time)
+        #             outputs.append({"break": break_time})
+        #         else:
+        #             output = self.text2speech(parsed_input_text[i])
+        #             y = output['wav'].view(-1).cpu().tolist()
+        #             all_y.append(y)
+        #             durations = output['duration'].cpu().tolist()[1:]
+        #             all_durations.append(durations)
+        #             outputs.append({"speech": output})
+
+        # extract phonemes
+        phonemes = self.extract_phonemes(self.get_text(input_text))
+
+        final_y = []
+        for output in outputs:
+            if 'break' in output:
+                break_time = output['break']
+                final_y.extend([0.0] * int(np.floor(break_time * self.config.fs / 1000)))
+            elif 'speech' in output:
+                y = output['speech']['wav'].view(-1).cpu().numpy()
+                final_y.extend(y)
+        final_y = np.array(final_y)
+
+        audio_duration = (len(self._flatten(all_y)) / self.config.fs) * 1000
+        unit_duration = audio_duration / sum([sum(durations) for durations in all_durations])
+
+        all_ends = []
+        for durations in all_durations:
+            ends = np.cumsum(durations) * unit_duration
+            all_ends.append(ends)
+        breaks = [output['break'] for output in outputs if 'break' in output]
+        assert len(breaks) == len(all_ends) - 1
+
+        for i in range(len(all_ends) - 1):
+            all_ends[i + 1] += all_ends[i][-1] + breaks[i]
+
+        ends = np.concatenate(all_ends)
+
+        starts = [0] + ends[:-1].tolist()
+        lines = []
+        for phoneme, start, end in zip(phonemes, starts, ends):
+            phoneme, start, end = self.regulate_phoneme_duration(phoneme, start, end)
+            line = "{:4d} 0    0    0    0  {:4d} {:4s} 0.0000 ".format(start, end, phoneme) + '\n'
+            lines.append(line)
+        wav_file = os.path.join(self.config.voice_dir, "x.wav")
+        write(wav_file, self.text2speech.fs, final_y)
+        return {"phonemes": " ".join(lines), "wav": final_y}
+
 
 if __name__ == '__main__':
-    string = "Hey How are you doing today? I am happy."
-    vs = VoiceSynthesizer(config_file="configs/female_voice_config.json")
-    response = vs.tts(string)
+    # s = "This is the first sentence. <break time='2000ms'/> This is the second sentence. <break time='1s'/> This is the third sentence."
+    #s = "The state of CO has a lot of ski resorts."
+    #s = "These materials are NSFW."
+    #s = "The format is JPEG."
+    #s = "This is just a sentence. I don't know how to answer this question. <break time='1s'/> This is the second sentence. The state of CO has a lot of ski resorts."
+    #s = "CBS Sports has the latest NFL Football news."
+    #s = "۷Chapter VI of the book is about the history of the United States. <break time='5s'/> The book is about the history of the United States."
+    #s = "The city of hamedan is located in Iran."
+    #s = "The NASDAK told me to go away."
+    s = "this is <say-as interpret-as='acronym'>AMD </say-as> stands for Advanced Micro Devices. Also <say-as interpret-as='acronym'>XMR</say-as> is irrelevant. <break time='1s'/> This is the second sentence."
+    vs = VoiceSynthesizer(config_file="configs/ryan_config.json")
+    response = vs.tts2(s)
     print(response)
